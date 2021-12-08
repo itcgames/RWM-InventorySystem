13e0e09be4a400c836d3ff11d0f3d38bacb10ca2
Jack Malone <44910666+JackMalone1@users.noreply.github.com>
2021-12-08 17:52:15 +0000

    Complete Task Jira[QN-5]
    * added in opening and closing of the inventory as well as returning the current active item
    
    * adding different commands for each of the things that the player is able to do
    
    * added in some extra functions to be able to set the correct command
    
    * starting to write tests
    
    * fixed test for using item
    
    * Complete Task Jira[QN-5]

--- Packages/rwm.inventory/Runtime/Inventory.cs
+++ Packages/rwm.inventory/Runtime/Inventory.cs
@@ -14,12 +14,39 @@ public class Inventory : MonoBehaviour
     [Tooltip("Font that the inventory will use to display the UI. Only needed if Use Default Display is set to true.")]
     private Font _font;
     private List<GameObject> _items;
+    private List<GameObject> _usedItems;
+    private uint _activeItemIndex;
+    private bool _isOpen;
+    private const string _notSetString = "not set";
+    private string _openCommand = _notSetString;
+    private string _closeCommand = _notSetString;
+    private string _submitCommand = _notSetString;
 
     [HideInInspector]
     public uint MaxStackAmount { get => _maxStackAmount; }
 
     [HideInInspector]
     public List<GameObject> Items { get => _items; }
+
+    [HideInInspector]
+    public List<GameObject> UsedItems { get => _usedItems;}
+
+    [HideInInspector]
+    public GameObject ActiveItem { get {
+            if (_items == null || _activeItemIndex > _items.Count) return null;
+            return _items[(int)_activeItemIndex];
+    }}
+
+    public bool IsOpen { get => _isOpen; set => _isOpen = value; }
+
+    [HideInInspector]
+    public string OpenCommand { get => _openCommand; set => _openCommand = value; }
+
+    [HideInInspector]
+    public string CloseCommand { get => _closeCommand; set => _closeCommand = value; }
+
+    [HideInInspector]
+    public string SubmitCommand { get => _submitCommand; set => _submitCommand = value; }
 
     public void SetMaxStackAmount(uint stackAmount)
     {
@@ -28,6 +55,11 @@ public class Inventory : MonoBehaviour
             if (stackAmount < _items.Count) return;
         }
         _maxStackAmount = stackAmount;
+    }
+
+    private void Start()
+    {
+        _usedItems = new List<GameObject>();
     }
 
 
@@ -52,11 +84,109 @@ public class Inventory : MonoBehaviour
         AddNewItemToInventory(newItem, amount);
     }
 
+    public void UseItem(string submitCommand)
+    {
+        if(Input.GetButtonDown(submitCommand) && _isOpen)
+        {
+            if (_items[(int)_activeItemIndex] == null) return;
+            InventoryItem item = _items[(int)_activeItemIndex].GetComponent<InventoryItem>();
+
+            if (item.useFunction != null)
+            {
+                item.useFunction();
+            }
+            else
+            {
+                if(_usedItems == null) _usedItems = new List<GameObject>();
+                _usedItems.Add(_items[(int)_activeItemIndex]);
+            }
+            item.NumberOfItems--;
+            if(item.NumberOfItems <= 0)
+            {
+                _items.Remove(_items[(int)_activeItemIndex]);
+                if (_items.Count < _activeItemIndex && _items.Count > 0) _activeItemIndex--;
+            }
+        }
+    }
+
+    public void UseItem()
+    {
+        if (_isOpen)
+        {
+            if (_items[(int)_activeItemIndex] == null) return;
+            InventoryItem item = _items[(int)_activeItemIndex].GetComponent<InventoryItem>();
+
+            if(item.useFunction != null)
+            {
+                Debug.Log("has function");
+                item.useFunction();
+            }
+            else
+            {
+                if (_usedItems == null) _usedItems = new List<GameObject>();
+                _usedItems.Add(_items[(int)_activeItemIndex]);
+            }
+            item.NumberOfItems--;
+            if (item.NumberOfItems <= 0)
+            {
+                _items.Remove(_items[(int)_activeItemIndex]);
+                if (_items.Count - 1 < _activeItemIndex && _items.Count > 0) _activeItemIndex--;
+                Debug.Log("Removing used item from inventory");
+            }
+        }
+    }
+
+    public void OpenInventory(string openCommand)
+    {
+        if(Input.GetButtonDown(openCommand))
+        {
+            _isOpen = true;
+        }
+    }
+
+    public void OpenInventory()
+    {
+        _isOpen = true;
+        Debug.Log("Inventory Opened");
+    }
+
+    public void CloseInventory(string closeCommand)
+    {
+        if(Input.GetButtonDown(closeCommand))
+        {
+            _isOpen = false;
+        }
+    }
+
+    public void CloseInventory()
+    {
+        _isOpen = false;
+        Debug.Log("Inventory Opened");
+    }
+
+    
+
+    void SetSubmitCommand(string submitCommand)
+    {
+        _submitCommand = submitCommand;
+    }
+
+    void SetOpenCommand(string openCommand)
+    {
+        _openCommand = openCommand;
+    }
+
+    void SetCloseCommand(string closeCommand)
+    {
+        _closeCommand = closeCommand;
+    }
+
     private void AddFirstItemToInventory(GameObject newItem, uint amount)
     {
         _items = new List<GameObject>();
         newItem.GetComponent<InventoryItem>().NumberOfItems = amount;
         _items.Add(newItem);
+        _activeItemIndex = 0;
     }
 
     private void AddNewItemToInventory(GameObject newItem, uint amount)
@@ -64,6 +194,7 @@ public class Inventory : MonoBehaviour
         if (1 + _items.Count > _maxStackAmount) return;//don't add to the inventory when it's full
         newItem.GetComponent<InventoryItem>().NumberOfItems = amount;
         _items.Add(newItem);
+        _activeItemIndex = (uint)_items.Count - 1;
     }
 
     private GameObject FindLastAddedStackOfItem(InventoryItem item)
--- Packages/rwm.inventory/Tests/InventoryTests.cs
+++ Packages/rwm.inventory/Tests/InventoryTests.cs
@@ -183,6 +183,54 @@ namespace Tests
         }
         #endregion
 
+        #region Use Items Tests
+        [UnityTest]
+        public IEnumerator AddUsedItemToList()
+        {
+            Inventory inventory = new Inventory();
+            inventory.SetMaxStackAmount(1);
+            yield return new WaitForSeconds(0.1f);
+            GameObject item = CreateItem(5, true, "item");
+            inventory.AddItem(item, 1);
+            inventory.OpenInventory();
+            inventory.UseItem();
+            yield return new WaitForSeconds(0.1f);
+            Assert.IsNotNull(inventory.UsedItems);
+            Assert.AreEqual(1, inventory.UsedItems.Count);
+            Assert.AreEqual(0, inventory.Items.Count);//remove item from inventory after it is used
+        }
+
+        [UnityTest]
+        public IEnumerator DontUseItemWhenClosed()
+        {
+            Inventory inventory = new Inventory();
+            inventory.SetMaxStackAmount(1);
+            yield return new WaitForSeconds(0.1f);
+            GameObject item = CreateItem(5, true, "item");
+            inventory.AddItem(item, 1);
+            inventory.UseItem();
+            yield return new WaitForSeconds(0.1f);
+            Assert.IsNull(inventory.UsedItems);
+            Assert.AreEqual(1, inventory.Items.Count);
+        }
+
+        [UnityTest]
+        public IEnumerator UseFunctionIfAble()
+        {
+            Inventory inventory = new Inventory();
+            inventory.SetMaxStackAmount(1);
+            yield return new WaitForSeconds(0.1f);
+            GameObject item = CreateItem(5, true, "item");
+            item.GetComponent<InventoryItem>().useFunction += UseItem;
+            inventory.AddItem(item, 1);
+            inventory.OpenInventory();
+            inventory.UseItem();
+            yield return new WaitForSeconds(0.1f);
+            Assert.IsNull(inventory.UsedItems);
+            Assert.AreEqual(0, inventory.Items.Count);
+        }
+        #endregion
+
         #region helper functions
         private GameObject CreateItem(uint maxItemsPerStack, bool isStackable, string itemName)
         {
@@ -193,6 +241,11 @@ namespace Tests
             script.Name = itemName;
             script.NumberOfItems = 0;
             return item;
+        }
+
+        public void UseItem()
+        {
+            Debug.Log("using an item in the tests");
         }
         #endregion
     }
